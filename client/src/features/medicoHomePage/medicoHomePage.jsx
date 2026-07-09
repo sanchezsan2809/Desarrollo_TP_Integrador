8ac996225f078113a193856fe11c83f17378df1b
npannunzio <nicopanu2900@gmail.com>
2026-07-09 20:07:37 -0300

    cambio de fonts

--- client/src/features/medicoHomePage/medicoHomePage.jsx
+++ client/src/features/medicoHomePage/medicoHomePage.jsx
@@ -24,7 +24,7 @@ export default function MedicoDashboard() {
                 <Typography
                     variant="h4"
                     sx={{ 
-                        fontFamily: '"Sour Gummy", sans-serif',
+                        fontFamily: '"Capriola", sans-serif',
                         borderRadius: '18px',
                         textTransform: 'none',
                         mb: 2
